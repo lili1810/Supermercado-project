575af0612903c9e9d5b286b47fb76ff05bbeb083
lili1810 <cavalcante.annalivia@gmail.com>
2025-09-17 15:38:07 -0300

    troca de pessoa para cliente

--- Supermercado_Project/src/ControleCarrinho.java
+++ Supermercado_Project/src/ControleCarrinho.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
-public class ControleCarrinho {
-}
+public class ControleCarrinho
+{
+    private Carrinho carrinho;
+
+    public void acoesCarrinho(Produto produto,String nome, int quantidade,char sinal)
+    {
+        if(carrinho.Checar(nome))
+        {
+            if(sinal == '+')
+            {
+                produto.adicionarEstoque(quantidade);
+                System.out.println("adicionado!");
+            }
+            else if (sinal == '-')
+            {
+                produto.reduzirEstoque(quantidade);
+                System.out.println("adicionado!");
+            }
+            else
+            {
+                System.out.println("");
+            }
+        }
+
+    }
+}/*
+carrinho
+    adicionar produto (nome quantidade)
+    retirar produto(nome quantidade)
+*/
